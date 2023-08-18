ee1e40533c9e977720149b0a88c4768b9c6db12f
jamshedhasanjonov <jamshedhasanjonov@gmail.com>
2023-08-18 23:53:14 +0500

    add new style and some js code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const main = document.getElementById('main')
 const button = document.getElementById("random");
 const title = document.getElementById("title");
-
+const randomBtn = document.getElementById('random')
 const randomGradient = () => {
   const direction = Math.floor(Math.random() * 360);  
   const r1 = Math.floor(Math.random() * 360)
@@ -15,6 +15,7 @@ const randomGradient = () => {
 
   const background = main.style.background = `
   linear-gradient(${direction}deg,rgba(${r1},${g1},${b1},${a1}),rgba(${r2},${g2},${b2},${a2})) ,linear-gradient(${direction+1}deg,rgba(${r1+1},${g1+1},${b1+1},${a1+1}),rgba(${r2+1},${g2+1},${b2+1},${a2+1}))`;
+  randomBtn.style.background =background;
   title.textContent = `${background};`;
 }
 button.addEventListener("click", randomGradient);
